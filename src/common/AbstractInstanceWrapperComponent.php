e2228c5e2634e2a80f733e2ca9479e954734a110
Andreas Prucha (Abexto/Helicon) <andreas.prucha@gmail.com>
2018-04-18 16:17:54 +0200

    Introduced AbstractInstanceWrapperComponent

---
+++ src/common/AbstractInstanceWrapperComponent.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Copyright 2018 Andreas Prucha, Abexto - Helicon Software Development.
+ */
+
+namespace abexto\amylian\yii2\base\common;
+
+/**
+ * Description of AbstractInstanceWrapperComponent
+ *
+ * @author Andreas Prucha, Abexto - Helicon Software Development
+ */
+class AbstractInstanceWrapperComponent
+{
+    //put your code here
+}
