93e929f94b7f8f88a985f7a578a8981df939f61d
Andreas Prucha (Abexto/Helicon) <andreas.prucha@gmail.com>
2018-04-18 16:54:13 +0200

    Init AbstractInstanceWrapperComponent

--- src/common/AbstractInstanceWrapperComponent.php
+++ src/common/AbstractInstanceWrapperComponent.php
@@ -11,7 +11,47 @@ namespace abexto\amylian\yii2\base\common;
  *
  * @author Andreas Prucha, Abexto - Helicon Software Development
  */
-class AbstractInstanceWrapperComponent
+Abstract class AbstractInstanceWrapperComponent extends yii\base\Component
 {
-    //put your code here
+    /**
+     *
+     * @var string Class of the instance to wrap
+     */
+    public $instClass = null;
+    
+    /**
+     *
+     * @var array|null Parameters to be passed to constructor 
+     */
+    public $constructorArgs = null;
+
+    /**
+     * @var object|null Wrapped object instance
+     */
+    protected $_inst = null;
+    
+    /**
+     * Creates the wrapped instance
+     * @return object
+     */
+    protected function createNewInst()
+    {
+        $classReflection = new ReflectionClass($this->instClass);
+        if ($this->constructorArgs === null) {
+            return $classReflection->newInstance();
+        } else {
+            return $classReflection->newInstanceArgs($this->concstructorArgs);
+        }
+    }
+    
+    /**
+     * Returns the wrapped object instance
+     */
+    public function getInst()
+    {
+      if (!isset($this->_inst)) {
+          $this->inst = $this->createNewInst();
+      }
+      return $this->inst;
+    }
 }
